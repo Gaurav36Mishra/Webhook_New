181bb153d65716853961351caa58e475f3fafad4
Gaurav36Mishra <gaurav05mishra@gmail.com>
2018-09-06 15:27:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,28 +5,11 @@ const bodyParser = require("body-parser");
 
 const restService = express();
 
-const {
-  dialogflow,
-  Image,
-} = require('actions-on-google')
-
 restService.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
-
-// Create an app instance
-const app = dialogflow()
-
-// app.intent('Default Welcome Intent', conv => {
-//   conv.ask('Hi, how is it going?')
-//   conv.ask(`Here's a picture of a cat`)
-//   conv.ask(new Image({
-//     url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-//     alt: 'A cat',
-//   }))
-// })
 
 restService.use(bodyParser.json());
 
